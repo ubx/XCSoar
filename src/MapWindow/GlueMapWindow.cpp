ubx/XCSoar

commit 276ed25dd79fed25ee7d18379fcd4134d6e5599b

Topography: move the topography loader to a dedicated thread
Load shapes in background, while the map continues to be rendered.

src/MapWindow/GlueMapWindow.cpp
@@ -29,9 +29,11 @@ Copyright_License {
 #include "Interface.hpp"
 #include "Time/PeriodClock.hpp"
 #include "Event/Idle.hpp"
+#include "Topography/Thread.hpp"
 
 GlueMapWindow::GlueMapWindow(const Look &look)
   :MapWindow(look.map, look.traffic),
+   topography_thread(nullptr),
    logger(nullptr),
    idle_robin(-1),
 #ifdef ENABLE_OPENGL
@@ -57,6 +59,25 @@ GlueMapWindow::GlueMapWindow(const Look &look)
 GlueMapWindow::~GlueMapWindow()
 {
   Destroy();
+}
+
+void
+GlueMapWindow::SetTopography(TopographyStore *_topography)
+{
+  if (topography_thread != nullptr) {
+    topography_thread->LockStop();
+    delete topography_thread;
+    topography_thread = nullptr;
+  }
+
+  MapWindow::SetTopography(_topography);
+
+  if (_topography != nullptr)
+    topography_thread =
+      new TopographyThread(*_topography,
+                           [this](){
+                             SendUser(unsigned(Command::INVALIDATE));
+                           });
 }
 
 void
@@ -213,7 +234,6 @@ GlueMapWindow::Idle()
   clock.Update();
 
   bool still_dirty;
-  bool topography_dirty = true; /* scan topography in every Idle() call */
   bool terrain_dirty = true;
   bool weather_dirty = true;
 
@@ -221,7 +241,6 @@ GlueMapWindow::Idle()
     idle_robin = (idle_robin + 1) % 3;
     switch (idle_robin) {
     case 0:
-      topography_dirty = UpdateTopography(1) > 0;
       break;
 
     case 1:
@@ -233,7 +252,7 @@ GlueMapWindow::Idle()
       break;
     }
 
-    still_dirty = terrain_dirty || topography_dirty || weather_dirty;
+    still_dirty = terrain_dirty || weather_dirty;
   } while (!clock.Check(700) && /* stop after 700ms */
 #ifndef ENABLE_OPENGL
            !draw_thread->IsTriggered() &&
@@ -242,4 +261,16 @@ GlueMapWindow::Idle()
            still_dirty);
 
   return still_dirty;
+}
+
+bool
+GlueMapWindow::OnUser(unsigned id)
+{
+  switch (Command(id)) {
+  case Command::INVALIDATE:
+    Invalidate();
+    return true;
+  }
+
+  return false;
 }

build/main.mk
@@ -505,6 +505,7 @@ XCSOAR_SOURCES := \
 	$(SRC)/Topography/TopographyStore.cpp \
 	$(SRC)/Topography/TopographyFileRenderer.cpp \
 	$(SRC)/Topography/TopographyRenderer.cpp \
+	$(SRC)/Topography/Thread.cpp \
 	$(SRC)/Topography/TopographyGlue.cpp \
 	$(SRC)/Topography/XShape.cpp \
 	$(SRC)/Topography/CachedTopographyRenderer.cpp \

build/test.mk
@@ -1807,6 +1807,7 @@ RUN_MAP_WINDOW_SOURCES = \
 	$(SRC)/Task/RoutePlannerGlue.cpp \
 	$(SRC)/Topography/TopographyFile.cpp \
 	$(SRC)/Topography/TopographyStore.cpp \
+	$(SRC)/Topography/Thread.cpp \
 	$(SRC)/Topography/TopographyFileRenderer.cpp \
 	$(SRC)/Topography/TopographyRenderer.cpp \
 	$(SRC)/Topography/TopographyGlue.cpp \

src/MapWindow/GlueMapWindow.hpp
@@ -39,6 +39,7 @@ Copyright_License {
 struct Look;
 struct GestureLook;
 class Logger;
+class TopographyThread;
 
 class OffsetHistory
 {
@@ -57,6 +58,12 @@ public:
 
 
 class GlueMapWindow : public MapWindow {
+  enum class Command {
+    INVALIDATE,
+  };
+
+  TopographyThread *topography_thread;
+
   const Logger *logger;
 
   unsigned idle_robin;
@@ -147,6 +154,8 @@ public:
   GlueMapWindow(const Look &look);
   virtual ~GlueMapWindow();
 
+  void SetTopography(TopographyStore *_topography);
+
   void SetLogger(Logger *_logger) {
     logger = _logger;
   }
@@ -215,6 +224,7 @@ protected:
   virtual void OnPaint(Canvas &canvas) override;
   virtual void OnPaintBuffer(Canvas& canvas) override;
   virtual bool OnTimer(WindowTimer &timer) override;
+  bool OnUser(unsigned id) override;
 
   /**
    * This event handler gets called when a gesture has
@@ -243,8 +253,13 @@ private:
 
   void SaveDisplayModeScales();
 
+  void OnProjectionModified();
+
   void UpdateScreenAngle();
   void UpdateProjection();
+
+public:
+  void SetLocation(const GeoPoint location);
 
   /**
    * Update the visible_projection location, but only if the new
@@ -254,7 +269,6 @@ private:
    */
   void SetLocationLazy(const GeoPoint location);
 
-public:
   void UpdateMapScale();
 
   /**

src/MapWindow/GlueMapWindowDisplayMode.cpp
@@ -23,6 +23,7 @@ Copyright_License {
 
 #include "GlueMapWindow.hpp"
 #include "Terrain/RasterTerrain.hpp"
+#include "Topography/Thread.hpp"
 #include "Interface.hpp"
 #include "Profile/Profile.hpp"
 #include "Screen/Layout.hpp"
@@ -110,9 +111,18 @@ GlueMapWindow::PanTo(const GeoPoint &location)
 }
 
 void
+GlueMapWindow::OnProjectionModified()
+{
+  if (topography_thread != nullptr &&
+      CommonInterface::GetMapSettings().topography_enabled)
+    topography_thread->Trigger(visible_projection);
+}
+
+void
 GlueMapWindow::SetMapScale(fixed scale)
 {
   MapWindow::SetMapScale(scale);
+  OnProjectionModified();
 
   const bool circling =
     CommonInterface::GetUIState().display_mode == DisplayMode::CIRCLING;
@@ -137,6 +147,7 @@ GlueMapWindow::RestoreMapScale()
   visible_projection.SetScale(settings.circle_zoom_enabled && circling
                               ? settings.circling_scale
                               : settings.cruise_scale);
+  OnProjectionModified();
 }
 
 inline void
@@ -209,6 +220,8 @@ GlueMapWindow::UpdateScreenAngle()
     visible_projection.SetScreenAngle(
       basic.track_available ? basic.track : Angle::Zero());
 
+  OnProjectionModified();
+
   compass_visible = orientation != MapOrientation::NORTH_UP;
 }
 
@@ -249,7 +262,16 @@ GlueMapWindow::UpdateMapScale()
 
     visible_projection.SetFreeMapScale(distance);
     settings.cruise_scale = visible_projection.GetScale();
+
+    OnProjectionModified();
   }
+}
+
+void
+GlueMapWindow::SetLocation(const GeoPoint location)
+{
+  MapWindow::SetLocation(location);
+  OnProjectionModified();
 }
 
 void
@@ -346,4 +368,6 @@ GlueMapWindow::UpdateProjection()
     SetLocation(terrain->GetTerrainCenter());
 
   visible_projection.UpdateScreenBounds();
+
+  OnProjectionModified();
 }

src/MapWindow/GlueMapWindowEvents.cpp
@@ -33,6 +33,7 @@ Copyright_License {
 #include "Pan.hpp"
 #include "Util/Clamp.hpp"
 #include "Event/Idle.hpp"
+#include "Topography/Thread.hpp"
 
 #ifdef USE_X11
 #include "Event/Globals.hpp"
@@ -46,6 +47,9 @@ Copyright_License {
 void
 GlueMapWindow::OnDestroy()
 {
+  /* stop the TopographyThread */
+  SetTopography(nullptr);
+
 #ifdef ENABLE_OPENGL
   data_timer.Cancel();
 #endif

src/Topography/Thread.cpp
@@ -1,0 +1,77 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2014 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#include "Thread.hpp"
+#include "TopographyStore.hpp"
+#include "Thread/Util.hpp"
+
+TopographyThread::TopographyThread(TopographyStore &_store,
+                                   std::function<void()> &&_callback)
+  :StandbyThread("Topograpghy"),
+   store(_store),
+   callback(_callback),
+   last_bounds(GeoBounds::Invalid()) {}
+
+TopographyThread::~TopographyThread()
+{
+}
+
+void
+TopographyThread::Trigger(const WindowProjection &_projection)
+{
+  const GeoBounds new_bounds = _projection.GetScreenBounds();
+  if (last_bounds.IsValid() && last_bounds.IsInside(new_bounds))
+    /* the cache is still fresh */
+    return;
+
+  last_bounds = new_bounds.Scale(fixed(1.1));
+
+  {
+    const ScopeLock protect(mutex);
+    next_projection = _projection;
+    StandbyThread::Trigger();
+  }
+}
+
+void
+TopographyThread::Tick()
+{
+  // TODO: call only once
+  SetIdlePriority();
+
+  bool again = true;
+  while (next_projection.IsValid() && again && !IsStopped()) {
+    const WindowProjection projection = next_projection;
+
+    mutex.Unlock();
+    again = store.ScanVisibility(projection, 1) > 0;
+    mutex.Lock();
+  }
+
+  /* notify the client that we have updated the topography cache */
+  if (callback) {
+    mutex.Unlock();
+    callback();
+    mutex.Lock();
+  }
+}

src/Topography/Thread.hpp
@@ -1,0 +1,60 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2014 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_TOPOGRAPHY_THREAD_HPP
+#define XCSOAR_TOPOGRAPHY_THREAD_HPP
+
+#include "Thread/StandbyThread.hpp"
+#include "Projection/WindowProjection.hpp"
+#include "Geo/GeoBounds.hpp"
+
+#include <functional>
+
+class TopographyStore;
+
+/**
+ * A thread that loads topography files asynchronously.
+ */
+class TopographyThread final : private StandbyThread {
+  TopographyStore &store;
+
+  const std::function<void()> callback;
+
+  WindowProjection next_projection;
+
+  GeoBounds last_bounds;
+
+public:
+  TopographyThread(TopographyStore &_store, std::function<void()> &&_callback);
+  ~TopographyThread();
+
+  using StandbyThread::LockStop;
+
+  void Trigger(const WindowProjection &_projection);
+
+private:
+  /* virtual methods from class StandbyThread*/
+  void Tick() override;
+};
+
+#endif

commit a09847dec932b390b4254930227434b565d4807f

GlueMapWindow: forward unknown OnUser() events to base class
Fixes lost DoubleBufferWindow::INVALIDATE events; regression by commit
276ed25

src/MapWindow/GlueMapWindow.cpp
@@ -274,7 +274,8 @@ GlueMapWindow::OnUser(unsigned id)
     draw_thread->TriggerRedraw();
 #endif
     return true;
-  }
 
-  return false;
+  default:
+    return MapWindow::OnUser(id);
+  }
 }