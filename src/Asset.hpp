ubx/XCSoar

commit cdf4dff0590f717254f6329b5f081165bf97c507

Asset: Implement IsEmbedded(), HasTouchScreen() and HasCursorKeys() correctly for iOS

src/Asset.hpp
@@ -220,7 +220,7 @@ constexpr
 static inline bool
 IsEmbedded()
 {
-  return IsAndroid() || IsWindowsCE() || IsKobo();
+  return IsAndroid() || IsWindowsCE() || IsKobo() || IsIOS();
 }
 
 /**
@@ -275,7 +275,7 @@ constexpr
 static inline bool
 HasTouchScreen()
 {
-  return IsAndroid() || (IsWindowsCE() && !IsAltair()) || IsKobo();
+  return IsAndroid() || (IsWindowsCE() && !IsAltair()) || IsKobo() || IsIOS();
 }
 
 /**
@@ -307,7 +307,7 @@ HasCursorKeys()
 #ifdef ANDROID
   return has_cursor_keys;
 #else
-  return !IsKobo();
+  return !IsKobo() || !IsIOS();
 #endif
 }
 

commit 796532e94a97b24b9ff315394c9a80f954e9727e

Asset: fix HasCursorKeys() breakage
Regression by commit cdf4dff0

src/Asset.hpp
@@ -307,7 +307,7 @@ HasCursorKeys()
 #ifdef ANDROID
   return has_cursor_keys;
 #else
-  return !IsKobo() || !IsIOS();
+  return !IsKobo() && !IsIOS();
 #endif
 }
 