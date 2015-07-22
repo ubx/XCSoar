rdunning0823/tophat

Units: add Mass unit.  kg or lb
Set defaults for countries

src/Profile/ProfileKeys.hpp
@@ -33,6 +33,7 @@ extern const char TemperatureUnitsValue[];
 extern const char LiftUnitsValue[];
 extern const char PressureUnitsValue[];
 extern const char WingLoadingUnitsValue[];
+extern const char MassUnitsValue[];
 extern const char TaskSpeedUnitsValue[];
 extern const char DisplayUpValue[];
 extern const char OrientationCruise[];

ubx/XCSoar

src/Profile/ProfileKeys.hpp
@@ -35,6 +35,7 @@ extern const char TemperatureUnitsValue[];
 extern const char LiftUnitsValue[];
 extern const char PressureUnitsValue[];
 extern const char WingLoadingUnitValue[];
+extern const char MassUnitValue[];
 extern const char TaskSpeedUnitsValue[];
 extern const char DisplayUpValue[];
 extern const char OrientationCruise[];