rdunning0823/tophat

dlgBasicSettings.cpp: show wing loading units (metric or standard)

src/Profile/ProfileKeys.hpp
@@ -32,6 +32,7 @@ extern const char AltitudeUnitsValue[];
 extern const char TemperatureUnitsValue[];
 extern const char LiftUnitsValue[];
 extern const char PressureUnitsValue[];
+extern const char WingLoadingUnitsValue[];
 extern const char TaskSpeedUnitsValue[];
 extern const char DisplayUpValue[];
 extern const char OrientationCruise[];

ubx/XCSoar

src/Profile/ProfileKeys.hpp
@@ -34,6 +34,7 @@ extern const char AltitudeUnitsValue[];
 extern const char TemperatureUnitsValue[];
 extern const char LiftUnitsValue[];
 extern const char PressureUnitsValue[];
+extern const char WingLoadingUnitValue[];
 extern const char TaskSpeedUnitsValue[];
 extern const char DisplayUpValue[];
 extern const char OrientationCruise[];