ubx/XCSoar

commit 3c33542898a57ef1ef6fbd8dde9850330025aa51

Enhancement WP ETA VMG
In the attachment the revised program code for WP_ETA_VMG (remove
unnecessary code, use standard time format/conversion)

Description: This new infobox shows the "Estimated arrival time at next
waypoint, assuming current ground  speed is maintained"

src/InfoBoxes/Content/Factory.cpp
@@ -849,6 +849,15 @@ static constexpr MetaData meta_data[] = {
     next_waypoint_infobox_panels,
   },
 
+  // e_WP_ETA_VMG
+  {
+    N_("Next waypoint arrival time (ground speed)"),
+    N_("WP ETA VMG"),
+    N_("Estimated arrival time at next waypoint, assuming current ground speed is maintained."),
+    UpdateInfoBoxNextETAVMG,
+    next_waypoint_infobox_panels,
+  },
+
   // e_Horizon
   {
     N_("Attitude indicator"),

po/de.po
@@ -2566,6 +2566,25 @@ msgstr ""
 "Geschätzte Zeit die benötigt wird den nächsten Wegpunkt zu erreichen, "
 "vorausgesetzt die aktuelle Flugleistung wird beibehalten."
 
+#: src/InfoBoxes/Content/Factory.cpp:854
+#, no-c-format
+msgid "Next waypoint arrival time (ground speed)"
+msgstr "Ankunftzeit am nächsten Wegpunkt (Geschw. ü. Gnd.)"
+
+#: src/InfoBoxes/Content/Factory.cpp:855
+#, no-c-format
+msgid "WP ETA VMG"
+msgstr "WP ETA VMG"
+
+#: src/InfoBoxes/Content/Factory.cpp:856
+#, no-c-format
+msgid ""
+"Estimated arrival time at next waypoint, assuming current ground "
+"speed is maintained."
+msgstr ""
+"Geschätzte Ankunftzeit am nächsten Wegpunkt, "
+"vorausgesetzt die aktuelle Flugleistung wird beibehalten."
+
 #: src/InfoBoxes/Content/Factory.cpp:1063
 #, no-c-format
 msgid "Attitude indicator"

src/InfoBoxes/Content/Task.cpp
@@ -777,6 +777,38 @@ UpdateInfoBoxNextETEVMG(InfoBoxData &data)
 }
 
 void
+UpdateInfoBoxNextETAVMG(InfoBoxData &data)
+{
+  const NMEAInfo &basic = CommonInterface::Basic();
+  const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
+
+  if (!basic.ground_speed_available || !task_stats.task_valid ||
+      !task_stats.current_leg.remaining.IsDefined()) {
+    data.SetInvalid();
+    return;
+  }
+
+  const auto d = task_stats.current_leg.remaining.GetDistance();
+  const auto v = basic.ground_speed;
+
+  if (!task_stats.task_valid ||
+      d <= 0 ||
+      v <= 0) {
+    data.SetInvalid();
+    return;
+  }
+
+  const int dd = (int)(d/v);
+  const BrokenTime &now_local = CommonInterface::Calculated().date_time_local;
+  if (now_local.IsPlausible()) {
+    const BrokenTime t = now_local + dd;
+    data.UnsafeFormatValue(_T("%02u:%02u"), t.hour, t.minute);
+    data.UnsafeFormatComment(_T("%02u"), t.second);
+  }
+
+}
+
+void
 UpdateInfoBoxFinalETEVMG(InfoBoxData &data)
 {
   const NMEAInfo &basic = CommonInterface::Basic();

src/InfoBoxes/Content/Task.hpp
@@ -135,6 +135,9 @@ void
 UpdateInfoBoxNextETEVMG(InfoBoxData &data);
 
 void
+UpdateInfoBoxNextETAVMG(InfoBoxData &data);
+
+void
 UpdateInfoBoxFinalETEVMG(InfoBoxData &data);
 
 void

src/InfoBoxes/Content/Type.hpp
@@ -156,6 +156,7 @@ namespace InfoBoxFactory
 
     /* 110..119 */
     NEXT_ARROW,
+    e_WP_ETA_VMG, /* Estimated arrival time at next waypoint assuming current speed is maintained*/
 
     e_NUM_TYPES /* Last item */
   };

commit 401bb64afaf7f917d65f03da68759e63fa753426

InfoBoxes/Content/Factory: Fix InfoBox numbering order
Commit 3c33542898a57ef1ef6fbd8dde9850330025aa51 broke infobox
ordering. Move "WP ETA VMG" to the end of the list.

src/InfoBoxes/Content/Factory.cpp
@@ -849,15 +849,6 @@ static constexpr MetaData meta_data[] = {
     next_waypoint_infobox_panels,
   },
 
-  // e_WP_ETA_VMG
-  {
-    N_("Next waypoint arrival time (ground speed)"),
-    N_("WP ETA VMG"),
-    N_("Estimated arrival time at next waypoint, assuming current ground speed is maintained."),
-    UpdateInfoBoxNextETAVMG,
-    next_waypoint_infobox_panels,
-  },
-
   // e_Horizon
   {
     N_("Attitude indicator"),
@@ -1042,6 +1033,16 @@ static constexpr MetaData meta_data[] = {
        "or the target within the AAT sector for AAT tasks."),
     IBFHelper<InfoBoxContentNextArrow>::Create,
   },
+
+  // e_WP_ETA_VMG
+  {
+    N_("Next waypoint arrival time (ground speed)"),
+    N_("WP ETA VMG"),
+    N_("Estimated arrival time at next waypoint, assuming current ground speed is maintained."),
+    UpdateInfoBoxNextETAVMG,
+    next_waypoint_infobox_panels,
+  },
+
 };
 
 static_assert(ARRAY_SIZE(meta_data) == NUM_TYPES,