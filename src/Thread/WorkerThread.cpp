ubx/XCSoar

commit c666fd59613d57cb28e43f6adce932fe8a1aafad

Thread/WorkerThread: use Cond instead of Trigger

src/Thread/WorkerThread.cpp
@@ -22,7 +22,6 @@ Copyright_License {
 */
 
 #include "Thread/WorkerThread.hpp"
-#include "Thread/Trigger.hpp"
 #include "Time/PeriodClock.hpp"
 
 WorkerThread::WorkerThread(const char *_name,
@@ -37,35 +36,37 @@ WorkerThread::Run()
 {
   PeriodClock clock;
 
+  const ScopeLock lock(mutex);
+
   while (true) {
     /* wait for work */
-    event_trigger.Wait();
+    if (!trigger_flag) {
+      if (_CheckStoppedOrSuspended())
+        break;
+
+      trigger_cond.wait(mutex);
+    }
 
     /* got the "stop" trigger? */
     if (delay > 0
-        ? WaitForStopped(delay)
-        : CheckStoppedOrSuspended())
+        ? _WaitForStopped(delay)
+        : _CheckStoppedOrSuspended())
       break;
 
-    /* reset the trigger here, because our client might have called
-       Trigger() a few more times while we were suspended in
-       CheckStoppedOrSuspended() */
-    event_trigger.Reset();
-
-    if (IsCommandPending()) {
-      /* just in case we got another suspend/stop command after
-         CheckStoppedOrSuspended() returned and before the trigger got
-         reset: restore the trigger and skip this iteration, to fix
-         the race condition */
-      event_trigger.Signal();
+    if (!trigger_flag)
       continue;
+
+    trigger_flag = false;
+
+    {
+      const ScopeUnlock unlock(mutex);
+
+      /* do the actual work */
+      if (period_min > 0)
+        clock.Update();
+
+      Tick();
     }
-
-    /* do the actual work */
-    if (period_min > 0)
-      clock.Update();
-
-    Tick();
 
     unsigned idle = idle_min;
     if (period_min > 0) {
@@ -74,7 +75,7 @@ WorkerThread::Run()
         idle = period_min - elapsed;
     }
 
-    if (idle > 0 && WaitForStopped(idle))
+    if (idle > 0 && _WaitForStopped(idle))
       break;
   }
 }

src/Thread/WorkerThread.hpp
@@ -25,13 +25,13 @@ Copyright_License {
 #define XCSOAR_THREAD_WORKER_THREAD_HPP
 
 #include "Thread/SuspensibleThread.hpp"
-#include "Thread/Trigger.hpp"
 
 /**
  * A thread which performs regular work in background.
  */
 class WorkerThread : public SuspensibleThread {
-  ::Trigger event_trigger;
+  Cond trigger_cond;
+  bool trigger_flag = false;
 
   unsigned period_min, idle_min, delay;
 
@@ -54,20 +54,33 @@ public:
    * Wakes up the thread to do work, calls tick().
    */
   void Trigger() {
-    event_trigger.Signal();
+    const ScopeLock lock(mutex);
+    if (!trigger_flag) {
+      trigger_flag = true;
+      trigger_cond.signal();
+    }
   }
 
   /**
    * Suspend execution until Resume() is called.
    */
   void BeginSuspend() {
-    SuspensibleThread::BeginSuspend();
-    Trigger();
+    const ScopeLock lock(mutex);
+    _BeginSuspend();
+  }
+
+  /**
+   * Like BeginSuspend(), but expects the mutex to be locked already.
+   */
+  void _BeginSuspend() {
+    SuspensibleThread::_BeginSuspend();
+    trigger_cond.signal();
   }
 
   void Suspend() {
-    BeginSuspend();
-    WaitUntilSuspended();
+    const ScopeLock lock(mutex);
+    _BeginSuspend();
+    _WaitUntilSuspended();
   }
 
   /**
@@ -75,8 +88,9 @@ public:
    * synchronously for the thread to exit.
    */
   void BeginStop() {
-    SuspensibleThread::BeginStop();
-    Trigger();
+    const ScopeLock lock(mutex);
+    SuspensibleThread::_BeginStop();
+    trigger_cond.signal();
   }
 
 protected:

commit 6bdfa826b1c3b282fb78df7e3f0a824511023471

Thread/WorkerThread: check trigger_flag again after being suspended
The method SuspensibleThread::_CheckStoppedOrSuspended() may unlock
the mutex while the thread is suspended.  Meanwhile, another caller
may set trigger_flag with nobody waiting on trigger_cond.  Just right
after that, the WorkerThread may start waiting on the condition
variable, and nobody will ever wake it up again.

This regression was caused by commit c666fd59

src/Thread/WorkerThread.cpp
@@ -44,7 +44,11 @@ WorkerThread::Run()
       if (_CheckStoppedOrSuspended())
         break;
 
-      trigger_cond.wait(mutex);
+      /* check trigger_flag again to avoid a race condition, because
+         _CheckStoppedOrSuspended() may have unlocked the mutex while
+         we were suspended */
+      if (!trigger_flag)
+        trigger_cond.wait(mutex);
     }
 
     /* got the "stop" trigger? */