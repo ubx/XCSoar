ubx/XCSoar

commit 384abeaa24964994b1e674a02818f6ac10cce2d4

Startup: delete main window instance in Shutdown()
This should fix #3679.

src/Startup.cpp
@@ -668,6 +668,7 @@ Shutdown()
 
   LogFormat("Close Windows - main");
   main_window->Destroy();
+  delete main_window;
 
   CloseLanguageFile();
 

commit 3187c4ce9272b8b3c516919b016c69e609070cce

Startup: fix regression introduced with fix for #3679
Commit 384abea introduced a crash in Android due to access to a deleted object.
Setting the object pointer to nullptr after deleting fixes this.

src/Startup.cpp
@@ -669,6 +669,7 @@ Shutdown()
   LogFormat("Close Windows - main");
   main_window->Destroy();
   delete main_window;
+  CommonInterface::main_window = nullptr;
 
   CloseLanguageFile();
 