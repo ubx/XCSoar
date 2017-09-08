ubx/XCSoar

commit cdc5b6e0148274aa75e013b95770b2b1d4afe174

Makefile: set TARGET_IS_LINUX=y / TARGET_IS_DARWIN=n correctly when cross compiling for Linux
Until now, the variables TARGET_IS_LINUX was set for Unix targets except
Darwin when compiling on Linux, and TARGET_IS_DARWIN when compiling for
Unix on Darwin. This is not always correct, because it makes sense to
cross-compile for Linux on Darwin (e. g. for the KOBO target). Instead,
insure that TARGET_IS_LINUX is set to "y" for all Linux specific targets
(TARGET_IS_DARWIN was already explicitly set to "y" for all Darwin
targets).

build/targets.mk
@@ -185,6 +185,7 @@ ifeq ($(TARGET),PI)
   override TARGET = UNIX
   TCPREFIX := arm-linux-gnueabihf-
   PI ?= /opt/pi/root
+  TARGET_IS_LINUX = y
   TARGET_IS_PI = y
   TARGET_IS_ARM = y
   TARGET_IS_ARMHF = y
@@ -211,7 +212,7 @@ ifeq ($(TARGET),KOBO)
 endif
 
 ifeq ($(TARGET),NEON)
-  # Experimental target for generic ARMv7 with NEON
+  # Experimental target for generic ARMv7 with NEON on Linux
   override TARGET = UNIX
   ifeq ($(USE_CROSSTOOL_NG),y)
     HOST_TRIPLET = arm-unknown-linux-gnueabihf
@@ -222,6 +223,7 @@ ifeq ($(TARGET),NEON)
   ifneq ($(CLANG),y)
     TARGET_ARCH += -mcpu=cortex-a8
   endif
+  TARGET_IS_LINUX = y
   TARGET_IS_ARM = y
   TARGET_IS_ARMHF = y
   ARMV7 := y
@@ -271,15 +273,6 @@ ifeq ($(TARGET),IOS64)
   CLANG = y
   TARGET_ARCH += -miphoneos-version-min=$(IOS_MIN_SUPPORTED_VERSION) -arch arm64
   ASFLAGS += -arch arm64
-endif
-
-ifeq ($(TARGET),UNIX)
-  ifeq ($(HOST_IS_LINUX)$(TARGET_IS_DARWIN),yn)
-    TARGET_IS_LINUX := y
-  endif
-  ifeq ($(HOST_IS_DARWIN),y)
-    TARGET_IS_DARWIN := y
-  endif
 endif
 
 ifeq ($(TARGET),UNIX)

commit 2f212ea61372517a5f0759069d34bc32217165fb

Makefile: restore TARGET_IS_LINUX=y for native Linux builds
This was removed by commit cdc5b6e0148274aa75e013b95770b2b1d4afe174
which broke all native Linux builds.

build/targets.mk
@@ -274,6 +274,10 @@ ifeq ($(TARGET),IOS64)
 endif
 
 ifeq ($(TARGET),UNIX)
+  ifeq ($(HOST_IS_LINUX)$(TARGET_IS_DARWIN),yn)
+    TARGET_IS_LINUX := y
+  endif
+
   HAVE_POSIX := y
   HAVE_WIN32 := n
   HAVE_MSVCRT := n