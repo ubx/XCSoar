ubx/XCSoar

commit b74169363c6e369c56d2ea1c066815c4e94ce165

Form, ...: use double instead of fixed

src/Form/DataField/Float.cpp
@@ -30,13 +30,6 @@ Copyright_License {
 
 static bool DataFieldKeyUp = false;
 
-gcc_pure
-static fixed
-ParseString(const TCHAR *s)
-{
-  return fixed(ParseDouble(s));
-}
-
 int
 DataFieldFloat::GetAsInteger() const
 {
@@ -60,11 +53,11 @@ DataFieldFloat::GetAsDisplayString() const
 void
 DataFieldFloat::SetAsInteger(int Value)
 {
-  SetAsFloat(fixed(Value));
+  SetAsFloat(Value);
 }
 
 void
-DataFieldFloat::SetAsFloat(fixed Value)
+DataFieldFloat::SetAsFloat(double Value)
 {
   if (Value < mMin)
     Value = mMin;
@@ -79,53 +72,53 @@ DataFieldFloat::SetAsFloat(fixed Value)
 void
 DataFieldFloat::SetAsString(const TCHAR *Value)
 {
-  SetAsFloat(ParseString(Value));
+  SetAsFloat(ParseDouble(Value));
 }
 
 void
 DataFieldFloat::Inc()
 {
   // no keypad, allow user to scroll small values
-  if (mFine && mValue < fixed(0.95) && mStep >= fixed(0.5) &&
-      mMin >= fixed(0))
-    SetAsFloat(mValue + fixed(1) / 10);
+  if (mFine && mValue < 0.95 && mStep >= 0.5 &&
+      mMin >= 0)
+    SetAsFloat(mValue + 1 / 10);
   else
-    SetAsFloat(fixed(mValue + mStep * SpeedUp(true)));
+    SetAsFloat(mValue + mStep * SpeedUp(true));
 }
 
 void
 DataFieldFloat::Dec()
 {
   // no keypad, allow user to scroll small values
-  if (mFine && mValue <= fixed(1) && mStep >= fixed(0.5) &&
-      mMin >= fixed(0))
-    SetAsFloat(mValue - fixed(1) / 10);
+  if (mFine && mValue <= 1 && mStep >= 0.5 &&
+      mMin >= 0)
+    SetAsFloat(mValue - 1 / 10);
   else
-    SetAsFloat(fixed(mValue - mStep * SpeedUp(false)));
+    SetAsFloat(mValue - mStep * SpeedUp(false));
 }
 
-fixed
+double
 DataFieldFloat::SpeedUp(bool keyup)
 {
   if (keyup != DataFieldKeyUp) {
     mSpeedup = 0;
     DataFieldKeyUp = keyup;
     last_step.Update();
-    return fixed(1);
+    return 1;
   }
 
   if (!last_step.Check(200)) {
     mSpeedup++;
     if (mSpeedup > 5) {
       last_step.UpdateWithOffset(350);
-      return fixed(10);
+      return 10;
     }
   } else
     mSpeedup = 0;
 
   last_step.Update();
 
-  return fixed(1);
+  return 1;
 }
 
 void
@@ -135,7 +128,7 @@ DataFieldFloat::SetFromCombo(int iDataFieldIndex, const TCHAR *sValue)
 }
 
 void
-DataFieldFloat::AppendComboValue(ComboList &combo_list, fixed value) const
+DataFieldFloat::AppendComboValue(ComboList &combo_list, double value) const
 {
   TCHAR a[edit_format.capacity()], b[display_format.capacity()];
   _stprintf(a, edit_format, (double)value);
@@ -147,7 +140,7 @@ ComboList
 DataFieldFloat::CreateComboList(const TCHAR *reference_string) const
 {
   const auto reference = reference_string != nullptr
-    ? ParseString(reference_string)
+    ? ParseDouble(reference_string)
     : mValue;
 
   ComboList combo_list;

src/ActionInterface.cpp
@@ -122,7 +122,7 @@ ActionInterface::SetBallast(double ballast, bool to_devices)
   // send to external devices
   if (to_devices) {
     const Plane &plane = GetComputerSettings().plane;
-    if (positive(plane.dry_mass)) {
+    if (plane.dry_mass > 0) {
       auto overload = (plane.dry_mass + ballast * plane.max_ballast) /
         plane.dry_mass;
 

src/ApplyExternalSettings.cpp
@@ -44,9 +44,9 @@ BallastProcessTimer()
   last_fraction = settings.ballast_fraction_available;
 
   if (settings.ballast_overload_available.Modified(last_overload) &&
-      settings.ballast_overload >= fixed(1) &&
-      positive(plane.max_ballast)) {
-    auto fraction = ((settings.ballast_overload - fixed(1)) *
+      settings.ballast_overload >= 1 &&
+      plane.max_ballast > 0) {
+    auto fraction = ((settings.ballast_overload - 1) *
                      plane.dry_mass) / plane.max_ballast;
     ActionInterface::SetBallast(fraction, false);
     modified = true;

src/CalculationThread.cpp
@@ -46,7 +46,7 @@ CalculationThread::SetComputerSettings(const ComputerSettings &new_value)
 }
 
 void
-CalculationThread::SetScreenDistanceMeters(fixed new_value)
+CalculationThread::SetScreenDistanceMeters(double new_value)
 {
   ScopeLock protect(mutex);
   screen_distance_meters = new_value;

src/CalculationThread.hpp
@@ -51,7 +51,7 @@ class CalculationThread final : public WorkerThread {
 
   ComputerSettings settings_computer;
 
-  fixed screen_distance_meters;
+  double screen_distance_meters;
 
   /** Pointer to the GlideComputer that should be used */
   GlideComputer &glide_computer;
@@ -60,7 +60,7 @@ public:
   CalculationThread(GlideComputer &_glide_computer);
 
   void SetComputerSettings(const ComputerSettings &new_value);
-  void SetScreenDistanceMeters(fixed new_value);
+  void SetScreenDistanceMeters(double new_value);
 
   bool Start(bool suspended=false) {
     if (!WorkerThread::Start(suspended))

src/Form/DataField/Float.hpp
@@ -26,13 +26,12 @@ Copyright_License {
 
 #include "Number.hpp"
 #include "Time/PeriodClock.hpp"
-#include "Math/fixed.hpp"
 
 class DataFieldFloat final : public NumberDataField {
-  fixed mValue;
-  fixed mMin;
-  fixed mMax;
-  fixed mStep;
+  double mValue;
+  double mMin;
+  double mMax;
+  double mStep;
   PeriodClock last_step;
   uint8_t mSpeedup;
   bool mFine;
@@ -42,12 +41,12 @@ class DataFieldFloat final : public NumberDataField {
   mutable TCHAR mOutBuf[OUTBUFFERSIZE+1];
 
 protected:
-  fixed SpeedUp(bool keyup);
+  double SpeedUp(bool keyup);
 
 public:
   DataFieldFloat(const TCHAR *edit_format, const TCHAR *display_format,
-                 fixed _min, fixed _max, fixed _value,
-                 fixed _step, bool _fine,
+                 double _min, double _max, double _value,
+                 double _step, bool _fine,
                  DataFieldListener *listener=nullptr)
     :NumberDataField(Type::REAL, true, edit_format, display_format, listener),
      mValue(_value), mMin(_min), mMax(_max), mStep(_step),
@@ -58,31 +57,31 @@ public:
     unit = text;
   }
 
-  void Set(fixed _value) {
+  void Set(double _value) {
     mValue = _value;
   }
 
-  fixed GetAsFixed() const {
+  double GetAsFixed() const {
     return mValue;
   }
 
-  void SetMin(fixed v) {
+  void SetMin(double v) {
     mMin = v;
   }
 
-  void SetMax(fixed v) {
+  void SetMax(double v) {
     mMax = v;
   }
 
-  void SetStep(fixed v) {
+  void SetStep(double v) {
     mStep = v;
   }
 
-  fixed GetStep() const {
+  double GetStep() const {
     return mStep;
   }
 
-  void SetAsFloat(fixed Value);
+  void SetAsFloat(double Value);
 
   /* virtual methods from class DataField */
   void Inc() override;
@@ -96,7 +95,7 @@ public:
   void SetFromCombo(int iDataFieldIndex, const TCHAR *sValue) override;
 
 protected:
-  void AppendComboValue(ComboList &combo_list, fixed value) const;
+  void AppendComboValue(ComboList &combo_list, double value) const;
 };
 
 #endif

src/Form/DigitEntry.cpp
@@ -382,7 +382,7 @@ DigitEntry::SetValue(unsigned value)
 }
 
 void
-DigitEntry::SetValue(fixed value)
+DigitEntry::SetValue(double value)
 {
   // XXX implement
   SetValue((int)value);
@@ -449,14 +449,14 @@ DigitEntry::GetPositiveInteger() const
   return value;
 }
 
-fixed
+double
 DigitEntry::GetPositiveFractional() const
 {
   const int dp = FindDecimalPoint();
   if (dp < 0)
-    return fixed(0);
+    return 0;
 
-  fixed value = fixed(0);
+  double value = 0;
   unsigned factor = 10;
 
   for (unsigned i = dp + 1; i < length; ++i) {
@@ -465,7 +465,7 @@ DigitEntry::GetPositiveFractional() const
       continue;
 
     assert(c.value < 10);
-    value += fixed(c.value) / factor;
+    value += double(c.value) / factor;
     factor *= 10;
   }
 
@@ -489,25 +489,25 @@ DigitEntry::GetTimeValue() const
 }
 
 void
-DigitEntry::SetDigits(fixed degrees, CoordinateFormat format, bool isLatitude)
+DigitEntry::SetDigits(double degrees, CoordinateFormat format, bool isLatitude)
 {
   // Calculate half the last digit so that we round to the nearest
-  fixed roundingAdjustment = fixed(0);
+  double roundingAdjustment = 0;
   switch (format) {
   case CoordinateFormat::DD_DDDDD:
-    roundingAdjustment = fixed(0.5 * (1.0 / 100000));
+    roundingAdjustment = 0.5 * (1.0 / 100000);
     break;
 
   case CoordinateFormat::DDMM_MMM:
-    roundingAdjustment = fixed(0.5 * ( (1.0/60) / 1000));
+    roundingAdjustment = 0.5 * ( (1.0/60) / 1000);
     break;
 
   case CoordinateFormat::DDMMSS_S:
-    roundingAdjustment = fixed(0.5 * ( (1.0/3600) / 10));
+    roundingAdjustment = 0.5 * ( (1.0/3600) / 10);
     break;
 
   case CoordinateFormat::DDMMSS:
-    roundingAdjustment = fixed(0.5 * ( (1.0/3600) / 1));
+    roundingAdjustment = 0.5 * ( (1.0/3600) / 1);
     break;
 
   default:
@@ -657,7 +657,7 @@ DigitEntry::GetGeoAngle(CoordinateFormat format) const
   assert(columns[1].type == Column::Type::DIGIT ||
          columns[1].type == Column::Type::DIGIT19);
   assert(columns[2].type == Column::Type::DIGIT);
-  auto degrees = fixed(columns[1].value * 10 + columns[2].value);
+  auto degrees = columns[1].value * 10 + columns[2].value;
 
   // Read columns according to specified format
   /// \todo support UTM format
@@ -675,7 +675,7 @@ DigitEntry::GetGeoAngle(CoordinateFormat format) const
                 columns[5].value * 1000 +
                 columns[6].value * 100 +
                 columns[7].value * 10  +
-                columns[8].value        ) * fixed(1 / 100000.);
+                columns[8].value        ) / 100000.;
     break;
 
   case CoordinateFormat::DDMM_MMM:
@@ -687,10 +687,10 @@ DigitEntry::GetGeoAngle(CoordinateFormat format) const
     assert(columns[8].type == Column::Type::DIGIT);
     assert(columns[9].type == Column::Type::DIGIT);
     // Read minute columns
-    degrees += (columns[4].value * 10 + columns[5].value) * fixed(1 / 60.)
+    degrees += (columns[4].value * 10 + columns[5].value) / 60.
       +  (columns[7].value * 100 +
           columns[8].value * 10  +
-          columns[9].value        ) * fixed(1 / 60000.);
+          columns[9].value        ) / 60000.;
     break;
 
   case CoordinateFormat::DDMMSS_S:
@@ -702,9 +702,9 @@ DigitEntry::GetGeoAngle(CoordinateFormat format) const
     assert(columns[8].type == Column::Type::DIGIT);
     assert(columns[10].type == Column::Type::DIGIT);
     // Read minute and decimal second columns
-    degrees += (columns[4].value * 10 + columns[5].value) * fixed(1 / 60.)
-      +  (columns[7].value * 10 + columns[8].value) * fixed(1 / 3600.)
-      +  (columns[10].value                       ) * fixed(1 / 36000.);
+    degrees += (columns[4].value * 10 + columns[5].value) / 60.
+      +  (columns[7].value * 10 + columns[8].value) / 3600.
+      +  (columns[10].value                       ) / 36000.;
     break;
 
   case CoordinateFormat::UTM: /// \todo support UTM format
@@ -716,8 +716,8 @@ DigitEntry::GetGeoAngle(CoordinateFormat format) const
     assert(columns[7].type == Column::Type::DIGIT6);
     assert(columns[8].type == Column::Type::DIGIT);
     // Read minute and second columns
-    degrees += (columns[4].value * 10 + columns[5].value) * fixed(1 / 60.)
-      +  (columns[7].value * 10 + columns[8].value) * fixed(1 / 3600.);
+    degrees += (columns[4].value * 10 + columns[5].value) / 60.
+      +  (columns[7].value * 10 + columns[8].value) / 3600.;
     break;
   }
 
@@ -825,17 +825,17 @@ DigitEntry::GetUnsignedValue() const
   return GetPositiveInteger();
 }
 
-fixed
-DigitEntry::GetFixedValue() const
+double
+DigitEntry::GetDoubleValue() const
 {
-  fixed value = fixed(GetPositiveInteger()) + GetPositiveFractional();
+  double value = GetPositiveInteger() + GetPositiveFractional();
   return IsNegative() ? -value : value;
 }
 
 Angle
 DigitEntry::GetAngleValue() const
 {
-  return Angle::Degrees(GetFixedValue());
+  return Angle::Degrees(GetDoubleValue());
 }
 
 bool

src/Form/DigitEntry.hpp
@@ -26,7 +26,6 @@ Copyright_License {
 
 #include "Screen/PaintWindow.hpp"
 #include "Renderer/ButtonRenderer.hpp"
-#include "Math/fixed.hpp"
 
 #include <assert.h>
 #include <stdint.h>
@@ -188,7 +187,7 @@ public:
 
   void SetValue(int value);
   void SetValue(unsigned value);
-  void SetValue(fixed value);
+  void SetValue(double value);
   void SetValue(RoughTime value);
   void SetValue(Angle value);
 
@@ -199,7 +198,7 @@ public:
   unsigned GetUnsignedValue() const;
 
   gcc_pure
-  fixed GetFixedValue() const;
+  double GetDoubleValue() const;
 
   gcc_pure
   RoughTime GetTimeValue() const;
@@ -258,7 +257,7 @@ protected:
   unsigned GetPositiveInteger() const;
 
   gcc_pure
-  fixed GetPositiveFractional() const;
+  double GetPositiveFractional() const;
 
   void IncrementColumn(unsigned i);
   void DecrementColumn(unsigned i);
@@ -267,7 +266,7 @@ protected:
   int FindColumnAt(unsigned x) const;
 
 private:
-  void SetDigits(fixed degrees, CoordinateFormat format, bool isLatitude);
+  void SetDigits(double degrees, CoordinateFormat format, bool isLatitude);
 
 protected:
   void OnSetFocus() override;

commit 931355b852daf7b66099e9e11508bc430779ee42

Form/DataField/Float: fix 0.1 increments/decrements
Fixes Coverity issues 166990 and 167027, caused by commit b74169363c6e

src/Form/DataField/Float.cpp
@@ -81,7 +81,7 @@ DataFieldFloat::Inc()
   // no keypad, allow user to scroll small values
   if (mFine && mValue < 0.95 && mStep >= 0.5 &&
       mMin >= 0)
-    SetAsFloat(mValue + 1 / 10);
+    SetAsFloat(mValue + 0.1);
   else
     SetAsFloat(mValue + mStep * SpeedUp(true));
 }
@@ -92,7 +92,7 @@ DataFieldFloat::Dec()
   // no keypad, allow user to scroll small values
   if (mFine && mValue <= 1 && mStep >= 0.5 &&
       mMin >= 0)
-    SetAsFloat(mValue - 1 / 10);
+    SetAsFloat(mValue - 0.1);
   else
     SetAsFloat(mValue - mStep * SpeedUp(false));
 }