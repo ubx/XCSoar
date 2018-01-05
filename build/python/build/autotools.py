ubx/XCSoar

commit d3493fc30cf37c543250f1a36f58d9d63d979590

build/python: refactoring: introduce new class MakeProject
This introduces a the new class MakeProject, which is used as a base
class for all Makefile based thirdparty libraries.

build/python/build/autotools.py
@@ -1,8 +1,8 @@
 import os.path, subprocess, sys
 
-from build.project import Project
+from build.makeproject import MakeProject
 
-class AutotoolsProject(Project):
+class AutotoolsProject(MakeProject):
     def __init__(self, url, alternative_url, md5, installed, configure_args=[],
                  autogen=False,
                  cppflags='',
@@ -14,7 +14,7 @@ class AutotoolsProject(Project):
                  use_destdir=False,
                  make_args=[],
                  **kwargs):
-        Project.__init__(self, url, alternative_url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, alternative_url, md5, installed, **kwargs)
         self.configure_args = configure_args
         self.autogen = autogen
         self.cppflags = cppflags
@@ -22,7 +22,6 @@ class AutotoolsProject(Project):
         self.libs = libs
         self.shared = shared
         self.install_prefix = install_prefix
-        self.install_target = install_target
         self.use_destdir = use_destdir
         self.make_args = make_args
 
@@ -81,14 +80,15 @@ class AutotoolsProject(Project):
         subprocess.check_call(configure, cwd=build, env=toolchain.env)
         return build
 
+    def get_make_args(self, toolchain):
+        return MakeProject.get_make_args(self, toolchain) + self.make_args
+
+    def get_make_install_args(self, toolchain):
+        args = MakeProject.get_make_install_args(self, toolchain)
+        if self.use_destdir:
+            args += ['DESTDIR=' + toolchain.install_prefix]
+        return args
+
     def build(self, toolchain):
         build = self.configure(toolchain)
-
-        destdir = []
-        if self.use_destdir:
-            destdir = ['DESTDIR=' + toolchain.install_prefix]
-
-        subprocess.check_call(['/usr/bin/make', '--quiet', '-j12'] + self.make_args,
-                              cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', self.install_target] + destdir,
-                              cwd=build, env=toolchain.env)
+        MakeProject.build(self, toolchain, build)

build/python/build/lua.py
@@ -1,16 +1,14 @@
 import os.path, subprocess, shutil
 
-from build.project import Project
+from build.makeproject import MakeProject
 from build.quilt import push_all
 
-class LuaProject(Project):
+class LuaProject(MakeProject):
     def __init__(self, url, alternative_url, md5, installed,
                  **kwargs):
-        Project.__init__(self, url, alternative_url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, alternative_url, md5, installed, **kwargs)
 
-    def build(self, toolchain):
-        src = self.unpack(toolchain, out_of_tree=False)
-
+    def get_make_args(self, toolchain):
         cflags = toolchain.cflags + ' ' + toolchain.cppflags
 
         # hard-code lua_getlocaledecpoint() because
@@ -18,14 +16,21 @@ class LuaProject(Project):
         # version we're depnding on
         cflags += " \"-Dlua_getlocaledecpoint()='.'\""
 
-        subprocess.check_call(['/usr/bin/make', '-j12',
-                               'CC=' + toolchain.cc,
-                               'AR=' + toolchain.ar + ' rcu',
-                               'RANLIB=true',
-                               'MYCFLAGS=' + cflags,
-                               'MYLDFLAGS=' + toolchain.ldflags,
-                               'liblua.a'],
-                              cwd=os.path.join(src, 'src'), env=toolchain.env)
+        return MakeProject.get_make_args(self, toolchain) + [
+            'CC=' + toolchain.cc,
+            'AR=' + toolchain.ar + ' rcu',
+            'RANLIB=true',
+            'MYCFLAGS=' + cflags,
+            'MYLDFLAGS=' + toolchain.ldflags,
+            'liblua.a'
+        ]
+
+    def build(self, toolchain):
+        src = self.unpack(toolchain, out_of_tree=False)
+
+        wd = os.path.join(src, 'src')
+
+        MakeProject.build(self, toolchain, wd, False)
 
         includedir = os.path.join(toolchain.install_prefix, 'include')
         libdir = os.path.join(toolchain.install_prefix, 'lib')

build/python/build/makeproject.py
@@ -1,0 +1,28 @@
+import subprocess
+
+from build.project import Project
+
+class MakeProject(Project):
+    def __init__(self, url, alternative_url, md5, installed,
+                 install_target='install',
+                 **kwargs):
+        Project.__init__(self, url, alternative_url, md5, installed, **kwargs)
+        self.install_target = install_target
+
+    def get_simultaneous_jobs(self):
+        return 12
+
+    def get_make_args(self, toolchain):
+        return ['--quiet', '-j' + str(self.get_simultaneous_jobs())]
+
+    def get_make_install_args(self, toolchain):
+        return ['--quiet', self.install_target]
+
+    def make(self, toolchain, wd, args):
+        subprocess.check_call(['/usr/bin/make'] + args,
+                              cwd=wd, env=toolchain.env)
+
+    def build(self, toolchain, wd, install=True):
+        self.make(toolchain, wd, self.get_make_args(toolchain))
+        if install:
+            self.make(toolchain, wd, self.get_make_install_args(toolchain))

build/python/build/openssl.py
@@ -1,11 +1,21 @@
 import subprocess
 
-from build.project import Project
+from build.makeproject import MakeProject
 
-class OpenSSLProject(Project):
+class OpenSSLProject(MakeProject):
     def __init__(self, url, alternative_url, md5, installed,
                  **kwargs):
-        Project.__init__(self, url, alternative_url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, alternative_url, md5, installed, install_target='install_sw', **kwargs)
+
+    def get_simultaneous_jobs(self):
+        return 1
+
+    def get_make_args(self, toolchain):
+        return MakeProject.get_make_args(self, toolchain) + [
+            'CC=' + toolchain.cc,
+            'AR=' + toolchain.ar + ' r',
+            'RANLIB=arm-linux-gnueabihf-ranlib'
+        ]
 
     def build(self, toolchain):
         src = self.unpack(toolchain, out_of_tree=False)
@@ -14,13 +24,4 @@ class OpenSSLProject(Project):
                                'dist', 'no-dso', 'no-krb5',
                                '--prefix=' + toolchain.install_prefix],
                               cwd=src, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', '-j12',
-                               'CC=' + toolchain.cc,
-                               'AR=' + toolchain.ar + ' r',
-                               'RANLIB=arm-linux-gnueabihf-ranlib'],
-                              cwd=src, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', 'install_sw',
-                               'CC=' + toolchain.cc,
-                               'AR=' + toolchain.ar + ' r',
-                               'RANLIB=arm-linux-gnueabihf-ranlib'],
-                              cwd=src, env=toolchain.env)
+        MakeProject.build(self, toolchain, wd)

build/python/build/zlib.py
@@ -1,23 +1,26 @@
 import subprocess
 
-from build.project import Project
+from build.makeproject import MakeProject
 
-class ZlibProject(Project):
+class ZlibProject(MakeProject):
     def __init__(self, url, alternative_url, md5, installed,
                  **kwargs):
-        Project.__init__(self, url, alternative_url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, alternative_url, md5, installed, **kwargs)
+
+    def get_make_args(self, toolchain):
+        return MakeProject.get_make_args(self, toolchain) + [
+            'CC=' + toolchain.cc + ' ' + toolchain.cppflags + ' ' + toolchain.cflags,
+            'CPP=' + toolchain.cc + ' -E ' + toolchain.cppflags,
+            'AR=' + toolchain.ar,
+            'ARFLAGS=' + toolchain.arflags,
+            'RANLIB=' + toolchain.ranlib,
+            'LDSHARED=' + toolchain.cc + ' -shared',
+            'libz.a'
+        ]
 
     def build(self, toolchain):
         src = self.unpack(toolchain, out_of_tree=False)
 
         subprocess.check_call(['./configure', '--prefix=' + toolchain.install_prefix, '--static'],
                               cwd=src, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet',
-                               'CC=' + toolchain.cc + ' ' + toolchain.cppflags + ' ' + toolchain.cflags,
-                               'CPP=' + toolchain.cc + ' -E ' + toolchain.cppflags,
-                               'AR=' + toolchain.ar,
-                               'ARFLAGS=' + toolchain.arflags,
-                               'RANLIB=' + toolchain.ranlib,
-                               'LDSHARED=' + toolchain.cc + ' -shared',
-                               'install'],
-                              cwd=src, env=toolchain.env)
+        MakeProject.build(self, toolchain, src)

commit 548e5e526aed850606b9c01023b802e6b27e7c37

build/python/autotools: remove the unused "install_target" parameter
This fixes a regression from commit
d3493fc30cf37c543250f1a36f58d9d63d979590; after this commit,
"install_target" was eaten by the AutotoolsProject constructor,
instead of propagating it to MakeProject.

build/python/build/autotools.py
@@ -10,7 +10,6 @@ class AutotoolsProject(MakeProject):
                  libs='',
                  shared=False,
                  install_prefix=None,
-                 install_target='install',
                  use_destdir=False,
                  make_args=[],
                  config_script='configure',