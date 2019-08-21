458ee13fe7fcbff03bea8e55d4b7d4170e039019
andreas <andreas.luethi@gmx.net>
2019-08-21 12:41:10 +0200

    Device/Driver: Add set CAN speed.

--- src/Device/Port/CANPort.cpp
+++ src/Device/Port/CANPort.cpp
@@ -41,17 +41,20 @@ CANPort::~CANPort()
 }
 
 bool
-CANPort::Open(const char *port_name, unsigned baud_rate) {
+CANPort::Open(const char *_port_name, unsigned _baud_rate) {
 
-  sc = socket( PF_CAN, SOCK_RAW, CAN_RAW );
+    port_name = _port_name;
+    sc = socket( PF_CAN, SOCK_RAW, CAN_RAW );
 
   struct ifreq ifr;
-  strcpy(ifr.ifr_name, port_name);
+  strcpy(ifr.ifr_name, _port_name);
   int ret = ioctl(sc, SIOCGIFINDEX, &ifr);
 
   if(ret != 0){
     throw FormatErrno("Can not connect to %s", ifr.ifr_name);
   }
+
+  SetBaudrate(_baud_rate);
 
    // todo -- temporary, to be moved to CANaerospace.cpp!
   const std::vector<uint32_t> can_ids{
