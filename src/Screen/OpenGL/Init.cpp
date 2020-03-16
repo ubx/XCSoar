ubx/XCSoar

commit 67a9a193fa319029dc95b4f4c22c5e9d38a9884f

Screen/OpenGL: hard-code the use of GLSL
Drop the "classic" (shader-less) OpenGL code.  All of our targets are
capable of GLSL, so there's no reason to keep the old code around.

src/Screen/OpenGL/Init.cpp
@@ -39,19 +39,15 @@ Copyright_License {
 #include "Screen/EGL/System.hpp"
 #endif
 
-#ifdef USE_GLSL
 #include "Shaders.hpp"
 #include "Math/Angle.hpp"
-#endif
 
 #ifdef ANDROID
 #include "Android/Main.hpp"
 #include "Android/NativeView.hpp"
 #endif
 
-#ifdef USE_GLSL
 #include <glm/gtc/matrix_transform.hpp>
-#endif
 
 #include <algorithm>
 
@@ -272,15 +268,9 @@ OpenGL::SetupContext()
   glDisable(GL_LIGHTING);
 #endif
 
-#ifndef USE_GLSL
-  glEnableClientState(GL_VERTEX_ARRAY);
-#endif
-
   InitShapes();
 
-#ifdef USE_GLSL
   InitShaders();
-#endif
 }
 
 #ifdef SOFTWARE_ROTATE_DISPLAY
@@ -331,7 +321,6 @@ OpenGL::SetupViewport(UnsignedPoint2D size)
 
   glViewport(0, 0, size.x, size.y);
 
-#ifdef USE_GLSL
 #ifdef SOFTWARE_ROTATE_DISPLAY
   projection_matrix = glm::rotate(glm::mat4(1),
                                   (GLfloat)Angle::Degrees(OrientationToRotation(display_orientation)).Radians(),
@@ -340,22 +329,6 @@ OpenGL::SetupViewport(UnsignedPoint2D size)
 #endif
   projection_matrix = glm::ortho<float>(0, size.x, size.y, 0, -1, 1);
   UpdateShaderProjectionMatrix();
-#else
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-#ifdef SOFTWARE_ROTATE_DISPLAY
-  glRotatef(OrientationToRotation(display_orientation), 0, 0, 1);
-  OrientationSwap(size, display_orientation);
-#endif
-#ifdef HAVE_GLES
-  glOrthox(0, size.x << 16, size.y << 16, 0, -(1<<16), 1<<16);
-#else
-  glOrtho(0, size.x, size.y, 0, -1, 1);
-#endif
-
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-#endif
 
   viewport_size = size;
 
@@ -365,9 +338,7 @@ OpenGL::SetupViewport(UnsignedPoint2D size)
 void
 OpenGL::Deinitialise()
 {
-#ifdef USE_GLSL
   DeinitShaders();
-#endif
 
   DeinitShapes();
 

build/opengl.mk
@@ -31,7 +31,6 @@ OPENGL ?= n
 endif
 
 GLES2 ?= n
-GLSL ?= $(GLES2)
 
 ifeq ($(OPENGL),y)
 OPENGL_CPPFLAGS = -DENABLE_OPENGL
@@ -51,10 +50,7 @@ else
 OPENGL_LDLIBS = -lGL
 endif
 
-ifeq ($(GLSL),y)
-OPENGL_CPPFLAGS += -DUSE_GLSL
 OPENGL_CPPFLAGS += $(GLM_CPPFLAGS)
-endif
 
 # Needed for native VBO support
 OPENGL_CPPFLAGS += -DGL_GLEXT_PROTOTYPES

build/screen.mk
@@ -109,12 +109,8 @@ SCREEN_SOURCES += \
 	$(SCREEN_SRC_DIR)/OpenGL/Buffer.cpp \
 	$(SCREEN_SRC_DIR)/OpenGL/Shapes.cpp \
 	$(SCREEN_SRC_DIR)/OpenGL/Surface.cpp \
+	$(SCREEN_SRC_DIR)/OpenGL/Shaders.cpp \
 	$(SCREEN_SRC_DIR)/OpenGL/Triangulate.cpp
-
-ifeq ($(GLSL),y)
-SCREEN_SOURCES += \
-	$(SCREEN_SRC_DIR)/OpenGL/Shaders.cpp
-endif
 endif
 
 ifeq ($(ENABLE_SDL),y)

src/MapWindow/OverlayBitmap.cpp
@@ -155,15 +155,9 @@ MapOverlayBitmap::Draw(Canvas &canvas,
 
   const ScopeTextureConstantAlpha blend(use_bitmap_alpha, alpha);
 
-#ifdef USE_GLSL
   glEnableVertexAttribArray(OpenGL::Attribute::TEXCOORD);
   glVertexAttribPointer(OpenGL::Attribute::TEXCOORD, 2, GL_FLOAT, GL_FALSE,
                         0, coord);
-#else
-  const GLEnable<GL_TEXTURE_2D> scope;
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glTexCoordPointer(2, GL_FLOAT, 0, coord);
-#endif
 
   for (const auto &polygon : clipped) {
     const auto &ring = polygon.outer();
@@ -188,9 +182,5 @@ MapOverlayBitmap::Draw(Canvas &canvas,
     glDrawArrays(GL_TRIANGLE_FAN, 0, n);
   }
 
-#ifdef USE_GLSL
   glDisableVertexAttribArray(OpenGL::Attribute::TEXCOORD);
-#else
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-#endif
 }

src/Renderer/GeoBitmapRenderer.cpp
@@ -31,13 +31,8 @@ Copyright_License {
 #include "Screen/OpenGL/Scope.hpp"
 #include "Screen/OpenGL/VertexPointer.hpp"
 #include "Screen/OpenGL/BulkPoint.hpp"
-
-#ifdef USE_GLSL
 #include "Screen/OpenGL/Shaders.hpp"
 #include "Screen/OpenGL/Program.hpp"
-#else
-#include "Screen/OpenGL/Compatibility.hpp"
-#endif
 
 void
 DrawGeoBitmap(const RawBitmap &bitmap, PixelSize bitmap_size,
@@ -73,27 +68,15 @@ DrawGeoBitmap(const RawBitmap &bitmap, PixelSize bitmap_size,
     x1, y1,
   };
 
-#ifdef USE_GLSL
   OpenGL::texture_shader->Use();
   glEnableVertexAttribArray(OpenGL::Attribute::TEXCOORD);
   glVertexAttribPointer(OpenGL::Attribute::TEXCOORD, 2, GL_FLOAT, GL_FALSE,
                         0, coord);
-#else
-  const GLEnable<GL_TEXTURE_2D> scope;
-  OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glTexCoordPointer(2, GL_FLOAT, 0, coord);
-#endif
 
   glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-#ifdef USE_GLSL
   glDisableVertexAttribArray(OpenGL::Attribute::TEXCOORD);
   OpenGL::solid_shader->Use();
-#else
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-#endif
 }
 
 #endif

src/Screen/Brush.hpp
@@ -134,11 +134,9 @@ public:
     color.Bind();
   }
 
-#ifdef USE_GLSL
   void BindUniform(GLint location) const {
     color.Uniform(location);
   }
-#endif
 #endif /* OPENGL */
 };
 

src/Screen/Icon.cpp
@@ -29,12 +29,8 @@ Copyright_License {
 #include "Screen/OpenGL/Texture.hpp"
 #include "Screen/OpenGL/Scope.hpp"
 
-#ifdef USE_GLSL
 #include "Screen/OpenGL/Shaders.hpp"
 #include "Screen/OpenGL/Program.hpp"
-#else
-#include "Screen/OpenGL/Compatibility.hpp"
-#endif
 #endif
 
 #include <algorithm>
@@ -114,12 +110,7 @@ MaskedIcon::Draw(Canvas &canvas, PixelPoint p) const
   p -= origin;
 
 #ifdef ENABLE_OPENGL
-#ifdef USE_GLSL
   OpenGL::texture_shader->Use();
-#else
-  const GLEnable<GL_TEXTURE_2D> scope;
-  OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-#endif
 
   const ScopeAlphaBlend alpha_blend;
 
@@ -148,30 +139,10 @@ MaskedIcon::Draw(Canvas &canvas, const PixelRect &rc, bool inverse) const
   const PixelPoint position = rc.CenteredTopLeft(size);
 
 #ifdef ENABLE_OPENGL
-#ifdef USE_GLSL
   if (inverse)
     OpenGL::invert_shader->Use();
   else
     OpenGL::texture_shader->Use();
-#else
-  const GLEnable<GL_TEXTURE_2D> scope;
-
-  if (inverse) {
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
-
-    /* invert the texture color */
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_COMBINE_RGB, GL_REPLACE);
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_SRC0_RGB, GL_TEXTURE);
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND0_RGB, GL_ONE_MINUS_SRC_COLOR);
-
-    /* copy the texture alpha */
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_COMBINE_ALPHA, GL_REPLACE);
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_SRC0_ALPHA, GL_TEXTURE);
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND0_ALPHA, GL_SRC_ALPHA);
-  } else
-    /* simple copy */
-    OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-#endif
 
   const ScopeAlphaBlend alpha_blend;
 

src/Screen/OpenGL/BufferCanvas.cpp
@@ -28,13 +28,8 @@ Copyright_License {
 #include "FrameBuffer.hpp"
 #include "RenderBuffer.hpp"
 #include "Init.hpp"
-
-#ifdef USE_GLSL
 #include "Shaders.hpp"
 #include "Program.hpp"
-#else
-#include "Compatibility.hpp"
-#endif
 
 #ifdef SOFTWARE_ROTATE_DISPLAY
 #include "DisplayOrientation.hpp"
@@ -139,15 +134,8 @@ BufferCanvas::Begin(Canvas &other)
     glPushAttrib(GL_VIEWPORT_BIT);
 #endif
 
-#ifdef USE_GLSL
     old_projection_matrix = OpenGL::projection_matrix;
     OpenGL::projection_matrix = glm::mat4(1);
-#else
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-#endif
 
     old_translate = OpenGL::translate;
     old_size = OpenGL::viewport_size;
@@ -161,10 +149,8 @@ BufferCanvas::Begin(Canvas &other)
     OpenGL::SetupViewport({GetWidth(), GetHeight()});
     OpenGL::translate = {0, 0};
 
-#ifdef USE_GLSL
     glVertexAttrib4f(OpenGL::Attribute::TRANSLATE,
                      OpenGL::translate.x, OpenGL::translate.y, 0, 0);
-#endif
   } else {
     offset = other.offset;
   }
@@ -200,23 +186,14 @@ BufferCanvas::Commit(Canvas &other)
     glPopAttrib();
 #endif
 
-#ifdef USE_GLSL
     OpenGL::projection_matrix = old_projection_matrix;
     OpenGL::UpdateShaderProjectionMatrix();
-#else
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glMatrixMode(GL_MODELVIEW);
-    glPopMatrix();
-#endif
 
     OpenGL::translate = old_translate;
     OpenGL::viewport_size = old_size;
 
-#ifdef USE_GLSL
     glVertexAttrib4f(OpenGL::Attribute::TRANSLATE,
                      OpenGL::translate.x, OpenGL::translate.y, 0, 0);
-#endif
 
 #ifdef SOFTWARE_ROTATE_DISPLAY
     OpenGL::display_orientation = old_orientation;
@@ -242,12 +219,7 @@ BufferCanvas::CopyTo(Canvas &other)
   assert(IsDefined());
   assert(!active || frame_buffer != nullptr);
 
-#ifdef USE_GLSL
   OpenGL::texture_shader->Use();
-#else
-  const GLEnable<GL_TEXTURE_2D> scope;
-  OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-#endif
 
   texture->Bind();
   texture->Draw(other.GetRect(), GetRect());

commit 695d368dbe7036f2736a37f20502040c1386e808

OpenGL/Init: Fix software screen rotation
This solves a regression introduced in 67a9a19.
Correctly multiply the rotation matrix defined by software screen rotation with other transformations, instead of overwriting it.

Fixes #351

src/Screen/OpenGL/Init.cpp
@@ -327,7 +327,7 @@ OpenGL::SetupViewport(UnsignedPoint2D size)
                                   glm::vec3(0, 0, 1));
   OrientationSwap(size, display_orientation);
 #endif
-  projection_matrix = glm::ortho<float>(0, size.x, size.y, 0, -1, 1);
+  projection_matrix *= glm::ortho<float>(0, size.x, size.y, 0, -1, 1);
   UpdateShaderProjectionMatrix();
 
   viewport_size = size;